e0aaad8801bd44f60a6aeeae3065abff523a6018
MOGAMAW <92805881+MogaMAW@users.noreply.github.com>
2022-10-30 18:31:19 +0300

    Add files via upload
    using while statement in java

---
+++ today.java
@@ -0,0 +1,26 @@
+public class today {
+    // for the key, it takes a variable to be used and we are going to use day
+   
+    public static void main(String[] args) {
+        
+        String day ="Friday";
+        switch (day) {
+            case Sunday:System.out.println("It's Sunday");
+            break;
+            case Monday:System.out.println("It's Monday");
+            break;
+            case Tuesday:System.out.println("It's Tuesday");
+            break;
+            case Wednesday:System.out.println("It's Wednesday");
+            break;
+            case:Thursday:System.out.println("It's Thursday");
+            break;
+            case Friday:System.out.println("It's Friday");
+            break;
+            case:Saturday:System.out.println("It's Saturday");
+            break;
+            default:System.out.println("It's not a day");
+            
+        }
+    }
+}
