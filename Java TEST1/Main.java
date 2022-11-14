9846efd5054f2cc63545e7447a5160cc7a03640f
MOGAMAW <92805881+MogaMAW@users.noreply.github.com>
2022-11-14 21:25:59 +0300

    Add files via upload

---
+++ Java TEST1/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+
+        student.setName("Alex Maroon");
+        student.setId("S21B24/001");
+        student.setGpa(4.89);
+
+    
+        System.out.println(student);
+    }
+}
---
+++ Java TEST1/Student.java
@@ -0,0 +1,46 @@
+
+public class Student{
+    private String name;
+    private String id;
+    private double gpa;
+    
+    public Student(){
+        name = null;
+        id = null;
+        gpa = 0.0;
+    }
+    public Student(String name, String id, double gpa){
+        this.name = name;
+        this.id = id;
+        this.gpa = gpa;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getId(){
+        return id;
+    }
+    public double getGpa(){
+        return gpa;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setId(String id){
+        this.id = id;
+    }
+    public void setGpa(Double gpa){
+        this.gpa = gpa;
+    }
+
+    public String toString(){
+        return "Name: " + name+  " ID: " +id  +  " GPA: " +gpa;
+    }
+    public boolean equals(Student anyother){
+        return name.equalsIgnoreCase(anyother.name) && gpa == anyother.gpa  && id.equalsIgnoreCase(anyother.id);
+    }
+
+}
+
+    
+
