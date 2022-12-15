dbd0e0117c973c4e4a47942108f6e54a625b463a
MOGAMAW <92805881+MogaMAW@users.noreply.github.com>
2022-12-15 20:22:47 +0300

    Add files via upload

---
+++ Resource.java
@@ -0,0 +1,32 @@
+public class Resource {
+    protected String CallID;
+    protected String Title;
+    protected boolean isBorrowed;
+
+    public Resource(String CallID, String tittle) {
+        this.CallID = CallID;
+        this.Title = tittle;
+        isBorrowed = false;
+    }
+
+    public void borrowed() {
+        isBorrowed = true;
+    }
+
+    public void returned() {
+        isBorrowed = false;
+
+    }
+
+    public String summary() {
+        String s = "";
+        s = "Call ID: " + CallID + "\nTitle :" + Title;
+        if (isBorrowed == false) {
+            s = s + " is in the library";
+        } else {
+            s = s + " is not in the library";
+        }
+
+        return s;
+    }
+}
