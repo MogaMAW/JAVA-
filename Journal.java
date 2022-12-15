fc4e38482449c7c384958593dfe9257116e225f7
MOGAMAW <92805881+MogaMAW@users.noreply.github.com>
2022-12-15 20:23:22 +0300

    Add files via upload

---
+++ Journal.java
@@ -0,0 +1,17 @@
+public class Journal extends Resource {
+
+    String Issue;
+
+    public Journal(String c, String t, String i) {
+        super(c, t);
+        Issue = i;
+    }
+
+    public String Summary() {
+        String s;
+        s = super.summary() + "\nIssue: " + Issue;
+
+        return s;
+    }
+
+}
