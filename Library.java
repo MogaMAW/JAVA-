b32d683a3acabf73523b0f3a6460fc889f7793d7
MOGAMAW <92805881+MogaMAW@users.noreply.github.com>
2023-01-10 22:10:15 +0300

    Update Library.java

--- Library.java
+++ Library.java
@@ -3,7 +3,7 @@ public class Library {
     public static void main(String[] args) {
         Resource resource = new Resource("QA76.1", "Euclid's geometry");
         Resource resource2 = new Resource("QA73.3", "Origin of the species");
-        Resource resource3 = new Journal("QA73.2", "ACM Transactions", "March 2007");
+        Resource resource3 = new Journal("QA73.2", "ACM Transactions", "March  2007");
 
         resource.isBorrowed = true;
         resource2.isBorrowed = true;
