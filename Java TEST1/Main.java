57abde8bdbed2b3bfee6987ecfad2d28f3602ffc
MOGAMAW <92805881+MogaMAW@users.noreply.github.com>
2023-12-28 00:09:13 +0300

    Update Main.java

--- Java TEST1/Main.java
+++ Java TEST1/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Student student = new Student();
 
-        student.setName("Alex Maroon");
+        student.setName("Alex Maroon Madueke");
         student.setId("S21B24/001");
         student.setGpa(4.89);
 
